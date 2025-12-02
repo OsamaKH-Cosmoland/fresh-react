2565c473a70c4f3e3a68e08cce8716d274898e0e
Osama Khaireldin <osamakhaireldin@gmail.com>
2025-12-03 01:56:57 +0200

    10Fix provider imports and HTTP wrappers for Vercel

--- api/orders.ts
+++ api/orders.ts
@@ -1,6 +1,6 @@
-import type { EmailProvider } from "../providers/emailProvider";
-import { FakeEmailProvider } from "../providers/fakeEmailProvider";
-import { GmailEmailProvider } from "../providers/gmailEmailProvider";
+import type { EmailProvider } from "./providers/emailProvider";
+import { FakeEmailProvider } from "./providers/fakeEmailProvider";
+import { GmailEmailProvider } from "./providers/gmailEmailProvider";
 import { buildOrdersHandler, notifyTestHandler, streamOrdersHandler } from "../lib/http/ordersHandler";
 import { enhanceApiResponse } from "../lib/http/responseHelpers";
 import { normalizeServerlessRequest } from "../lib/http/serverlessHelpers";
