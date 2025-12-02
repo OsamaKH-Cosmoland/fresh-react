ffb5fb1ab199f9a204251f9c9ffa07e417add2a6
Osama Khaireldin <osamakhaireldin@gmail.com>
2025-12-03 01:50:01 +0200

    9Fix provider imports and HTTP wrappers for Vercel

--- api/reviews.ts
+++ api/reviews.ts
@@ -1,6 +1,6 @@
-import reviewsHandler from "./http/reviewsHandler";
-import { enhanceApiResponse } from "./http/responseHelpers";
-import { normalizeServerlessRequest } from "./http/serverlessHelpers";
+import reviewsHandler from "../lib/http/reviewsHandler";
+import { enhanceApiResponse } from "../lib/http/responseHelpers";
+import { normalizeServerlessRequest } from "../lib/http/serverlessHelpers";
 
 export default async function handler(req: Parameters<typeof reviewsHandler>[0], res: Parameters<typeof reviewsHandler>[1]) {
   await normalizeServerlessRequest(req);
